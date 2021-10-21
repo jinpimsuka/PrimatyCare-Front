bc2d73e6659dad5557c2d454e168d60f6fe08b41
unknown <6131501006@lamduan.mfu.ac.th>
2021-10-22 04:04:58 +0700

    first-dev for demo

---
+++ pages/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>Bar Chart Example in Vue</h3>
+    <v-row justify="center" align="center">
+      <v-col md="6">
+        <v-card>
+          <bar-chart></bar-chart>
+        </v-card>
+      </v-col>
+      <v-spacer />
+
+      <v-col md="6" >
+        <v-card >
+          <h4 > จำนวนคนใน Type Area </h4>
+          <polar-area-chart></polar-area-chart>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import BarChart from "@/components/BarChart";
+import PolarAreaChart from '@/components/PolarChart'
+
+export default {
+  
+  components: {
+    BarChart,
+    PolarAreaChart,
+  },
+
+};
+</script>
